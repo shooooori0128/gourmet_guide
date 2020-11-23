6cf7362d6f27e89597ec8f987794379a2b19f90d
shooooori0128 <pf.shori0128@gmail.com>
2020-11-23 22:24:41 +0900

    ぐるなび処理を追記

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -23,10 +23,25 @@ class WebhookController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
+          # GoogleMapAPIから位置情報を取得
+          google_map_res = location_info(address_str: event.message['text'])
+
+          location = google_map_res.body['results'].first
+
+          # 経度・緯度を抽出
+          lat = location.dig('geometry', 'location', 'lat')
+          lng = location.dig('geometry', 'location', 'lng')
+
+          # ぐるなびAPIで位置情報にマッチするレストランの候補を出力
+          gnavi_res = suggest_restaurants(latitude: lat, longitude: lng)
+
+          first_shop = gnavi_res['rest'].first
+
           message = {
             type: 'text',
-            text: event.message['text']
+            text: first_shop.dig('name')
           }
+
           client.reply_message(event['replyToken'], message)
         end
       end
