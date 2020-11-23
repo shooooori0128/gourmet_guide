704f1dba784d3ac8e58c85211e24e4a913baecff
shooooori0128 <pf.shori0128@gmail.com>
2020-11-23 22:11:01 +0900

    webhookのテスト

--- Gemfile
+++ Gemfile
@@ -49,3 +49,5 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'rspec-rails', '~> 3.4'
 end
+
+gem 'line-bot-api'
--- app/controllers/api/v1/map_guides_controller.rb
+++
@@ -1,6 +0,0 @@
-##################################################
-# GoogleMap関連のAPI郡
-##################################################
-class Api::V1::MapGuidesController < ApplicationController
-  include GoogleMapHandler
-end
